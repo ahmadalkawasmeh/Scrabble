c89b6ddedcee329a80d5e9cca77c4eb1bc3358e5
dankuch <therealdkuch@gmail.com>
2022-10-20 17:35:27 -0400

    Added Game class -Added method bodies

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,37 @@
+import java.util.List;
+import java.util.Stack;
+
 public class Game {
+    private Board board;
+    private static List<Player> players;
+    private Player currentPlayer;
+    private Stack<Round> roundHistory;
+
+    public Game(int numPlayers){
+
+    }
+
+    private void initializeBoard(){
+
+    }
+
+    private void initializeLetterBag(){
+
+    }
+
+    private void initializePlayers(){
+
+    }
+
+    public void playRound(){
+
+    }
+
+    public void nextPlayer(){
+
+    }
+
+    public void getPlayerScores(){
+
+    }
 }
