068430988d23b64ebb51ba532ac1104a0113726c
james <jamesgrieder@cmail.carleton.ca>
2022-11-12 18:43:24 -0500

    Finished 2 test methods.  Added 2 additional test objects.  Started testSwapLettersCanSwapAllLetters (incomplete)

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -6,11 +6,13 @@ import static org.junit.Assert.*;
 
 public class PlayerTest {
 
-    Player p;
+    Player p, p2, p3;
 
     @Before
     public void setUp() throws Exception {
         p = new Player("Weird Al");
+        p2 = new Player("P2");
+        p3 = new Player("James");
     }
 
     @Test
@@ -33,13 +35,21 @@ public class PlayerTest {
 
     @Test
     public void testCanUpdateScoreByPositiveNumber() {
-        assertFalse(true);
+        assertTrue(p.getScore() == 0);
+        p.updateScore(25);
+        assertTrue(p.getScore() == 25);
+        p.updateScore(1);
+        assertTrue(p.getScore() == 26);
+        p.updateScore(7);
+        assertTrue(p.getScore() == 33);
     }
 
 
     @Test
-    public void testCannotUpdateScoreByNegativeNumber() {
-        assertFalse(true);
+    public void testCannotUpdateScoreByNegativeNumber() { // Actual fail --> need to update logic
+        assertTrue(p.getScore() == 0);
+        p.updateScore(-4);
+        assertFalse(p.getScore() == -4);
     }
 
 
@@ -98,7 +108,21 @@ public class PlayerTest {
 
 
     @Test
-    public void testSwapLettersCanSwapAllLetters() {
+    public void testSwapLettersCanSwapAllLetters() { // Incomplete
+        String testString1, testString2, testString3;
+        testString1 = "";
+        testString2 = "";
+        testString3 = "";
+
+
+        String tray1 = p.stringTray();
+        String tray2 = p2.stringTray();
+        String tray3 = p3.stringTray();
+
+
+
+        System.out.println(testString1);
+
         assertFalse(true);
     }
 
