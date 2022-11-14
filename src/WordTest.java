e79e9f72b5c3b48fea9783fd8c9ea91a0b240a22
dankuch <therealdkuch@gmail.com>
2022-11-13 20:41:32 -0500

    TrayTest

---
+++ src/WordTest.java
@@ -0,0 +1,44 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class WordTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void addLettersToMap() {
+    }
+
+    @Test
+    public void isHorizontal() {
+    }
+
+    @Test
+    public void findWordPosition() {
+    }
+
+    @Test
+    public void getWord() {
+    }
+
+    @Test
+    public void length() {
+    }
+
+    @Test
+    public void wordScore() {
+    }
+
+    @Test
+    public void getLetterPositions() {
+    }
+
+    @Test
+    public void hasValidBounds() {
+    }
+
+    @Test
+    public void numCoordinate() {
+    }
+}
