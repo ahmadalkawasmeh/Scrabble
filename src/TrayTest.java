a61cc9fd031564a511ae3e5b3babb5637e957e70
Daniel <danielkuchanski@cmail.carleton.ca>
2022-11-13 22:32:07 -0500

    TrayTest

---
+++ src/TrayTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class TrayTest {
+
+    Tray t;
+
+    @Before
+    public void setUp() throws Exception {
+        Tray t = new Tray();
+    }
+}
